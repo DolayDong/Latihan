db749e3e8c2388692c574cb61bf7e158e1007c95
Sapto Aji <saosabc321@gmail.com>
2020-01-06 15:28:19 +0700

    ngebenerin perhitungan jika input jamnya sama

--- latihan program parkir/App.java
+++ latihan program parkir/App.java
@@ -14,36 +14,44 @@ public class App {
             int jamMasuk = userInput.nextInt();
             System.out.println("Jam keluar : ");
             int jamKeluar = userInput.nextInt();
+            if (jamKeluar > 24) {
+                System.out.println("Jam keluar tidak valid");
+            } else if (jamMasuk > 24) {
+                System.out.println("Jam masuk tidak valid");
+            } else {
 
-            String jenisKendaraan = "Mobil";
-            int lamaParkir = jamKeluar - jamMasuk + 24;
-            switch (jenis) {
-            case 1:
-                jenisKendaraan = "Mobil";
-                int jamPertamaMobil = 5000;
-                int jamBerikutnyaMobil = 3000;
-                int hargaParkirMobil = jamPertamaMobil + (jamBerikutnyaMobil * lamaParkir - jamBerikutnyaMobil);
-                System.out.println("Terimakasih telah melakukan transaksi dnengan rincian sebagai berikut");
-                System.out.println("Plat nomor : " + plat);
-                System.out.println("Jenis kendaraan : " + jenisKendaraan);
-                System.out.println("Lama parkir : " + lamaParkir);
-                System.out.println("Biaya parkir : " + hargaParkirMobil);
-                System.out.println("Semoga pelayanan kami memuaskan");
-                break;
-            case 2:
-                jenisKendaraan = "Motor";
-                int jamPertamaMotor = 3000;
-                int jamBerikutnyaMotor = 1500;
-                int hargaParkirMotor = jamPertamaMotor + (jamBerikutnyaMotor * lamaParkir - jamBerikutnyaMotor);
-                System.out.println("Terimakasih telah melakukan transaksi dnengan rincian sebagai berikut");
-                System.out.println("Plat nomor : " + plat);
-                System.out.println("Jenis kendaraan : " + jenisKendaraan);
-                System.out.println("Lama parkir : " + lamaParkir);
-                System.out.println("Biaya parkir : " + hargaParkirMotor);
-                System.out.println("Semoga pelayanan kami memuaskan");
-            default:
-                System.out.println("Harap masukkan pilihan dengan benar");
-                break;
+                String jenisKendaraan = "Mobil";
+                int lamaParkir = jamKeluar - jamMasuk + 24;
+                switch (jenis) {
+                case 1:
+                    int hargaParkirMobil;
+                    jenisKendaraan = "Mobil";
+                    int jamPertamaMobil = 5000;
+                    int jamBerikutnyaMobil = 3000;
+                    hargaParkirMobil = jamPertamaMobil + (jamBerikutnyaMobil * lamaParkir - jamBerikutnyaMobil);
+                    System.out.println("Terimakasih telah melakukan transaksi dnengan rincian sebagai berikut");
+                    System.out.println("Plat nomor : " + plat);
+                    System.out.println("Jenis kendaraan : " + jenisKendaraan);
+                    System.out.println("Lama parkir : " + lamaParkir);
+                    System.out.println("Biaya parkir : " + hargaParkirMobil);
+                    System.out.println("Semoga pelayanan kami memuaskan");
+                    break;
+                case 2:
+                    int hargaParkirMotor;
+                    jenisKendaraan = "Motor";
+                    int jamPertamaMotor = 3000;
+                    int jamBerikutnyaMotor = 1500;
+                    hargaParkirMotor = jamPertamaMotor + (jamBerikutnyaMotor * lamaParkir - jamBerikutnyaMotor);
+                    System.out.println("Terimakasih telah melakukan transaksi dnengan rincian sebagai berikut");
+                    System.out.println("Plat nomor : " + plat);
+                    System.out.println("Jenis kendaraan : " + jenisKendaraan);
+                    System.out.println("Lama parkir : " + lamaParkir);
+                    System.out.println("Biaya parkir : " + hargaParkirMotor);
+                    System.out.println("Semoga pelayanan kami memuaskan");
+                default:
+                    System.out.println("Harap masukkan pilihan dengan benar");
+                    break;
+                }
             }
 
         }
